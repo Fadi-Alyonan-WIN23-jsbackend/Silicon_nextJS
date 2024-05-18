4f87176649e84f689d54536761c9dabeca178984
SebastianWirstrom <143399640+SebastianWirstrom@users.noreply.github.com>
2024-05-18 21:42:50 +0200

    started SingleCourse

--- app/courses/CourseList.tsx
+++ app/courses/CourseList.tsx
@@ -4,13 +4,14 @@ import React, { useEffect } from 'react';
 import { Course } from '../interfaces/coursesTypes';
 import styles from './CourseList.module.css';
 
+
 interface CourseListProps {
   courses: Course[];
 }
 
 const CourseList: React.FC<CourseListProps> = ({ courses }) => {
   useEffect(() => {
-    console.log('Rendering courses:', courses); // Logga de kurser som skickas till komponenten
+    console.log('Rendering courses:', courses); 
   }, [courses]);
 
   return (
@@ -22,33 +23,35 @@ const CourseList: React.FC<CourseListProps> = ({ courses }) => {
         <div className={`courseItems ${styles.courseItems}`}>
           {courses && courses.length > 0 ? (
             courses.map((item) => {
-              console.log('Rendering course item:', item); // Logga varje kurs för detaljerad kontroll
+              console.log('Rendering course item:', item); 
               return (
                 <div key={item.id} className={`course ${styles.course}`}>
-                  {item.isBestSeller && <div className={`bestSeller text-s ${styles.bestSeller}`}>Best Seller</div>}
-                  <img className={`courseImage ${styles.courseImage}`} src={item.imageUri} alt={item.title} />
-                  <div className={styles.content}>
-                    <div><h5 className={`courseTitle ${styles.courseTitle}`}>{item.title}</h5></div>
-                    <div className={`courseAuthor text-s ${styles.courseAuthor}`}>{item.author}</div>
-                    <div className={`coursePrice ${styles.coursePrice}`}>
-                      {item.prices.discount && item.prices.discount !== 0 ? (
-                        <>
-                          <div className={`discountPrice ${styles.discountPrice}`}>{item.prices.discount}</div>
-                          <div className={`originalPrice ${styles.lineThrough}`}>{item.prices.price}</div>
-                        </>
-                      ) : (
-                        <div>{item.prices.price}</div>
-                      )}
-                    </div>
-                    <hr className={styles.courseHr} />
-                    <div className={`courseFooter ${styles.courseFooter}`}>
-                      <div className={`courseHours ${styles.courseHours}`}><i className="fa-regular fa-clock"></i> {item.hours} hours</div>
-                      <div className="course-likes">
-                        <i className="fa-regular fa-thumbs-up"></i>
-                        {`${item.likesInPercent}% (${item.likes})`}
+                  <a href={`/SingleCourse${item.id}` }>
+                    {item.isBestSeller && <div className={`bestSeller text-s ${styles.bestSeller}`}>Best Seller</div>}
+                    <img className={`courseImage ${styles.courseImage}`} src={item.imageUri} alt={item.title} />
+                    <div className={styles.content}>
+                      <div><h5 className={`courseTitle ${styles.courseTitle}`}>{item.title}</h5></div>
+                      <div className={`courseAuthor text-s ${styles.courseAuthor}`}>{item.author}</div>
+                      <div className={`coursePrice ${styles.coursePrice}`}>
+                        {item.prices.discount && item.prices.discount !== 0 ? (
+                          <>
+                            <div className={`discountPrice ${styles.discountPrice}`}>{item.prices.discount} €</div>
+                            <div className={`originalPrice ${styles.lineThrough}`}>{item.prices.price} €</div>
+                          </>
+                        ) : (
+                          <div>{item.prices.price}</div>
+                        )}
+                      </div>
+                      <hr className={styles.courseHr} />
+                      <div className={`courseFooter ${styles.courseFooter}`}>
+                        <div className={`courseHours ${styles.courseHours}`}><i className="fa-regular fa-clock"></i> {item.hours} hours</div>
+                        <div className="course-likes">
+                          <i className="fa-regular fa-thumbs-up"></i>
+                          {`${item.likesInPercent} (${item.likes})`}
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </a>
                 </div>
               );
             })
